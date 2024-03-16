779062d1e204f8bb1372299b9a70ae7e6cfb0299
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-03-15 23:49:42 -0700

    edited-again

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,6 +9,7 @@ execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
+
 env.hosts = ['54.157.128.252', '54.89.178.220']
 
 
@@ -21,7 +22,8 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except Exception as e:
+        print(e)
         return None
 
 
@@ -34,6 +36,7 @@ def do_deploy(archive_path):
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
+        print("Packing web_static to {}".format(archive_path))
         run('mkdir -p {}{}/'.format(path, no_ext))
         run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
         run('rm /tmp/{}'.format(file_n))
@@ -41,8 +44,10 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        print("New version deployed!")
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 
@@ -52,3 +57,4 @@ def deploy():
     if archive_path is None:
         return False
     return do_deploy(archive_path)
+
